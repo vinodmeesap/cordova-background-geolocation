8ef1d4b0990afa7c272f9d50b94739d19d805db7
Chris Scott <christocracy@gmail.com>
2016-02-19 17:11:17 -0500

    Rename kReachabilityChangedNotification in TSReachability so that it doesn't conflict with others using the same popular Reachability component

--- src/ios/TSLocationManager.framework/Headers/LocationDAO.h
+++ src/ios/TSLocationManager.framework/Headers/LocationDAO.h
@@ -11,10 +11,9 @@
 
 @interface LocationDAO : NSObject
 {
+    sqlite3 *dbh;
     NSString *databasePath;
 }
-
-@property (nonatomic) sqlite3 *dbh;
 
 + (LocationDAO *)getInstance;
 - (BOOL) persist:(NSDictionary*)data timestamp:(NSDate*)timestamp;
