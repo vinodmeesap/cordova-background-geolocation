b682774f9096c428850758417ab2f50a3771b313
Chris Scott <christocracy@gmail.com>
2019-11-30 19:57:32 -0500

    [Added] new Authorization feature.  [Added] Encryption.  [Added] Demo server JWT support as well as local helper methods to request JWT from demo server.  [Added] New DeviceInfo module for providing simple device-info without need of 3rd party library

--- src/ionic/declarations/interfaces/ConnectivityChangeEvent.d.ts
+++ src/ionic/declarations/interfaces/ConnectivityChangeEvent.d.ts
@@ -5,7 +5,7 @@ declare module "cordova-background-geolocation" {
 	* @example
 	* ```typescript
 	* BackgroundGeolocation.onConnectivityChange(connectivityChangeEvent => {
-	*   console.log('[connectivitychange] ', connectivityChangeEvent.connected);
+	*   console.log("[connectivitychange] ", connectivityChangeEvent.connected);
 	* });
 	* ```
 	*/
